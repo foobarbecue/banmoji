a2e22d337a623042edefd83d2f40018f602f205b
Aaron Curtis <foobarbecue@gmail.com>
2021-12-04 23:02:34 -0800

    Add exporter, but not working yet

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-import { parseGIF, decompressFrames } from "gifuct-js";
+import { parseGIF, decompressFrames } from "gifuct-js"
+import GIF from "@coffeeb/gif"
 
+const gifExporter = new GIF({
+    worker: new Worker(new URL('./gif.worker.js', import.meta.url))
+})
 
-console.log('test')
+gifExporter.on('finished', function(blob) {
+    window.open(URL.createObjectURL(blob));
+});
+
 const uploadField = document.getElementsByTagName('input')[0]
-const uploadSpot = document.getElementById('uploadSpot')
 const gifcanvas = document.getElementsByTagName('canvas')[0] 
 const imgctx = gifcanvas.getContext('2d')
 // extra canvas needed to deal with gif transparency
 const tempcanvas = document.createElement('canvas')
 const tempctx = tempcanvas.getContext('2d')
 const overlay = document.querySelector('#overlay')
-let frames=null
+let frames = null
 let frameInd=0
 uploadField.onchange = async function (evt){
     const gif = parseGIF(await evt.target.files[0].arrayBuffer())
@@ -21,7 +27,7 @@ uploadField.onchange = async function (evt){
     tempcanvas.height = gifcanvas.height
     overlay.width = gifcanvas.width
     overlay.height = gifcanvas.height
-    drawFrames(frames)
+    drawFrames()
 }
 
 function drawFrames(){
@@ -31,12 +37,14 @@ function drawFrames(){
     tempctx.putImageData(imgdata, frame.dims.left, frame.dims.top)
     imgctx.drawImage(tempcanvas,0, 0)
     imgctx.drawImage(overlay, 0, 0, frames[0].dims.width, frames[0].dims.height)
+    gifExporter.addFrame(imgctx, {copy: true})
     frameInd++
     if (frameInd >= frames.length){
         frameInd = 0
+        gifExporter.render()
     }
     requestAnimationFrame(()=>{
-        setTimeout(drawFrames, 100)
+        setTimeout(drawFrames, frame.delay)
     })
 }
 
