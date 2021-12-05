cc7cc782a333a3ac4d414b2590c52ef62c44cff5
Aaron Curtis <foobarbecue@gmail.com>
2021-12-04 20:18:32 -0800

    Correctly scale "no" symbol

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const imgctx = gifcanvas.getContext('2d')
 // extra canvas needed to deal with gif transparency
 const tempcanvas = document.createElement('canvas')
 const tempctx = tempcanvas.getContext('2d')
-const myfilereader = new FileReader()
+const overlay = document.querySelector('#overlay')
 let frames=null
 let frameInd=0
 uploadField.onchange = async function (evt){
@@ -19,17 +19,18 @@ uploadField.onchange = async function (evt){
     gifcanvas.height = frames[0].dims.height
     tempcanvas.width = gifcanvas.width
     tempcanvas.height = gifcanvas.height
+    overlay.width = gifcanvas.width
+    overlay.height = gifcanvas.height
     drawFrames(frames)
 }
 
 function drawFrames(){
-    console.log('drawing')
     const frame = frames[frameInd]
     const imgdata = imgctx.createImageData(frame.dims.width, frame.dims.height)
     imgdata.data.set(frame.patch)
     tempctx.putImageData(imgdata, frame.dims.left, frame.dims.top)
     imgctx.drawImage(tempcanvas,0, 0)
-    imgctx.drawImage(document.querySelector('#banned'), 0, 0)
+    imgctx.drawImage(overlay, 0, 0, frames[0].dims.width, frames[0].dims.height)
     frameInd++
     if (frameInd >= frames.length){
         frameInd = 0
@@ -37,4 +38,8 @@ function drawFrames(){
     requestAnimationFrame(()=>{
         setTimeout(drawFrames, 100)
     })
+}
+
+function download(){
+    
 }
