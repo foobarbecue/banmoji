4e94bf2950f7c5b18644d1dcd7f99fe30893a061
Aaron Curtis <foobarbecue@gmail.com>
2021-12-05 20:40:12 -0800

    fix clearing problems and rename some vars

--- index.js
+++ index.js
@@ -4,41 +4,50 @@ import { GIFEncoder, quantize, applyPalette } from 'gifenc'
 const gifExporter = new GIFEncoder()
 
 const uploadField = document.getElementsByTagName('input')[0]
-const gifcanvas = document.getElementsByTagName('canvas')[0] 
-const imgctx = gifcanvas.getContext('2d')
-// extra canvas needed to deal with gif transparency
-const tempcanvas = document.createElement('canvas')
-const tempctx = tempcanvas.getContext('2d')
-const overlay = document.querySelector('#overlay')
-const downloadButton = document.getElementsByName("download")[0]
-let frames = null
+const gifDisplayCanvas = document.querySelector('canvas#gifdisplay') 
+const gifDisplayCtx = gifDisplayCanvas.getContext('2d')
+const origGifDisplayCanvas = document.querySelector('canvas#origgifdisplay')
+const origGifDisplayCtx = origGifDisplayCanvas.getContext('2d')
+
+const nonOverlainCanvas = document.createElement('canvas')
+
+const overlayEl = document.querySelector('#overlay')
+const inpImgDims = {width: null, height: null}
+const downloadButtonEl = document.getElementsByName("download")[0]
+let inpImgFrames = null
 let frameInd=0
 uploadField.onchange = async function (evt){
-    const gif = parseGIF(await evt.target.files[0].arrayBuffer())
-    frames = decompressFrames(gif, true)
-    gifcanvas.width = frames[0].dims.width
-    gifcanvas.height = frames[0].dims.height
-    tempcanvas.width = gifcanvas.width
-    tempcanvas.height = gifcanvas.height
-    overlay.width = gifcanvas.width
-    overlay.height = gifcanvas.height
+    const inpImgParsed = parseGIF(await evt.target.files[0].arrayBuffer())
+    inpImgFrames = decompressFrames(inpImgParsed, true)
+    // todo probably can save several LOC using unpacking or something
+    inpImgDims.width = inpImgFrames[0].dims.width
+    inpImgDims.height = inpImgFrames[0].dims.height
+    gifDisplayCanvas.width = inpImgDims.width
+    gifDisplayCanvas.height = inpImgDims.height
+    overlayEl.width = inpImgDims.width
+    overlayEl.height = inpImgDims.height
     
     // Now that we have an input image, set up the controls
-    downloadButton.onclick = render
-    downloadButton.style.display = 'block'
-    
+    downloadButtonEl.onclick = render
+    downloadButtonEl.style.display = 'block'
+    gifDisplayCanvas.style.display = 'block'
+    Object.assign(nonOverlainCanvas, inpImgDims)
+    Object.assign(origGifDisplayCanvas, inpImgDims)
     preview()
 }
 
 function render(){
-    for (const frame of frames) {
-        const imgdata = imgctx.createImageData(frame.dims.width, frame.dims.height)
+    for (const frame of inpImgFrames) {
+        const tempcanvas = document.createElement('canvas')
+        tempcanvas.width = gifDisplayCanvas.width
+        tempcanvas.height = gifDisplayCanvas.height
+        const imgdata = gifDisplayCtx.createImageData(frame.dims.width, frame.dims.height)
         imgdata.data.set(frame.patch)
         tempctx.putImageData(imgdata, frame.dims.left, frame.dims.top)
-        imgctx.drawImage(tempcanvas, 0, 0)
-        imgctx.drawImage(overlay, 0, 0, frames[0].dims.width, frames[0].dims.height)
+        gifDisplayCtx.drawImage(tempcanvas, 0, 0)
+        gifDisplayCtx.drawImage(overlayEl, 0, 0, inpImgFrames[0].dims.width, inpImgFrames[0].dims.height)
 
-        const imgdataForExport = imgctx.getImageData(0, 0, frames[0].dims.width, frames[0].dims.height).data
+        const imgdataForExport = gifDisplayCtx.getImageData(0, 0, inpImgFrames[0].dims.width, inpImgFrames[0].dims.height).data
 
         // Quantize your colors to a 256-color RGB palette palette
         const palette = quantize(imgdataForExport, 256)
@@ -46,7 +55,7 @@ function render(){
         // Get an indexed bitmap by reducing each pixel to the nearest color palette
         const indexed = applyPalette(imgdataForExport, palette)
 
-        gifExporter.writeFrame(indexed, frames[0].dims.width, frames[0].dims.height, {palette})
+        gifExporter.writeFrame(indexed, inpImgFrames[0].dims.width, inpImgFrames[0].dims.height, {palette})
     }
     gifExporter.finish()
     // Get a direct typed array view into the buffer to avoid copying it
@@ -54,23 +63,49 @@ function render(){
     download(buffer, 'animation.gif', { type: 'image/gif' })
 }
 
-function addOverlay(inpImgFrame, overlay){
-    const imgdata = imgctx.createImageData(inpImgFrame.dims.width, inpImgFrame.dims.height)
-    imgdata.data.set(inpImgFrame.patch)
-    tempctx.putImageData(imgdata, inpImgFrame.dims.left, inpImgFrame.dims.top)
-    imgctx.drawImage(tempcanvas,0, 0)
-    imgctx.drawImage(overlay, 0, 0, frames[0].dims.width, frames[0].dims.height)
-    return inpImgFrame
+function addOverlay(nonOverlainCanvas, newImgFrame, overlay){
+    // Inputs:
+    // non-overlain canvas nonOverlainCanvas where gif data is accumulating
+    // gifuct-js frame inpImgFrame
+    // overlay image element overlayEl
+    // 
+    
+    // create temporary canvas inpFrameCanvas and draw newImgFrame to it
+    const inpFrameCanvas = document.createElement('canvas')
+    Object.assign(inpFrameCanvas, inpImgDims)
+    const inpImgCtx = inpFrameCanvas.getContext('2d')
+    const inpImgData = gifDisplayCtx.createImageData(newImgFrame.dims.width, newImgFrame.dims.height)
+    inpImgData.data.set(newImgFrame.patch)
+    inpImgCtx.putImageData(inpImgData, newImgFrame.dims.left, newImgFrame.dims.top)
+
+    // draw inpFrameCanvas onto accumulating image nonOverlainCanvas
+    nonOverlainCanvas.globalAlpha = 1
+    nonOverlainCanvas.getContext('2d').drawImage(inpFrameCanvas, 0, 0)
+    
+    // create temporary canvas overlainCanvas
+    const overlainCanvas = document.createElement('canvas')
+    Object.assign(overlainCanvas, inpImgDims)
+    const overlainCtx = overlainCanvas.getContext('2d')
+    overlainCtx.globalAlpha = 1
+    overlainCtx.drawImage(nonOverlainCanvas,0, 0)
+    overlainCtx.globalAlpha = document.getElementById("nosignTransparency").value
+    overlainCtx.drawImage(overlay, 0, 0, inpImgFrames[0].dims.width, inpImgFrames[0].dims.height)
+    
+    return {original: nonOverlainCanvas, overlain: overlainCanvas}
 }
 
 function preview(){
-    const frame = addOverlay(frames[frameInd], overlay)
+    const newFrame = inpImgFrames[frameInd]
+    const overlayOutput = addOverlay(nonOverlainCanvas, newFrame, overlayEl)
+    gifDisplayCtx.clearRect(0, 0, inpImgDims.width, inpImgDims.height)
+    gifDisplayCtx.drawImage(overlayOutput.overlain, 0, 0)
+    origGifDisplayCtx.drawImage(overlayOutput.original, 0, 0)
     frameInd++
-    if (frameInd >= frames.length){
+    if (frameInd >= inpImgFrames.length){
         frameInd = 0
     }
     requestAnimationFrame(()=>{
-        setTimeout(preview, frame.delay)
+        setTimeout(preview, newFrame.delay)
     })
 }
 
